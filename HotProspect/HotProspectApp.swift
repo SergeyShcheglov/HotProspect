aa9e7a22570ddcbcea2206c0ae4a0e56382cf4e5
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-21 12:22:40 +0600

    Create HotProspect app

---
+++ HotProspect/HotProspectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectApp.swift
+//  HotProspect
+//
+//  Created by Sergey Shcheglov on 21.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
