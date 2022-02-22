af77abc8aec14e999febedeff01b05703f1da564
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-22 13:56:53 +0600

    Add enum for views

--- HotProspect/ProspectView.swift
+++ HotProspect/ProspectView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct ProspectView: View {
+    
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    
+    let filter: FilterType
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle(title)
+        }
+    }
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
     }
 }
 
 struct ProspectView_Previews: PreviewProvider {
     static var previews: some View {
-        ProspectView()
+        ProspectView(filter: .none)
     }
 }
