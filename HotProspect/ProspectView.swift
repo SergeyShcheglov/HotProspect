8bb3b75fc5400c255fc2a7996c8b4db2d4fbe612
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-22 13:47:20 +0600

    Create MeView

---
+++ HotProspect/ProspectView.swift
@@ -0,0 +1,20 @@
+//
+//  ProspectView.swift
+//  HotProspect
+//
+//  Created by Sergey Shcheglov on 22.02.2022.
+//
+
+import SwiftUI
+
+struct ProspectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProspectView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProspectView()
+    }
+}
