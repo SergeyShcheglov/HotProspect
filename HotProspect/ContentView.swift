fe68b2dc8fd1335f02fe41b525e626b61b5bad86
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-22 13:56:37 +0600

    Add TabView with 4 Views

--- HotProspect/ContentView.swift
+++ HotProspect/ContentView.swift
@@ -10,8 +10,26 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        VStack {
-
+        TabView {
+            ProspectView(filter: .none)
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            
+            ProspectView(filter: .contacted)
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            
+            ProspectView(filter: .uncontacted)
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.circle")
+                }
         }
     }
 }
