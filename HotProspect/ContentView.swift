a358221146f97512ad8b804de197ef2062786ca6
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-22 13:48:00 +0600

    Start project

--- HotProspect/ContentView.swift
+++ HotProspect/ContentView.swift
@@ -6,35 +6,12 @@
 //
 
 import SwiftUI
-import UserNotifications
 
 struct ContentView: View {
     
-    
     var body: some View {
         VStack {
-            Button("Request permission") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-                    if success {
-                        print("success")
-                    } else if let error = error {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-            
-            Button("Schedule notifications") {
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the dogs"
-                content.subtitle = "they look hungry"
-                content.sound = UNNotificationSound.default
-                
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
-                
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                
-                UNUserNotificationCenter.current().add(request)
-            }
+
         }
     }
 }
