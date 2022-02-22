991eae51f15cbd4def5b7d4cfeb155c8647a08b0
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-22 14:08:00 +0600

    Create Prospect class

---
+++ HotProspect/Prospect.swift
@@ -0,0 +1,23 @@
+//
+//  Prospect.swift
+//  HotProspect
+//
+//  Created by Sergey Shcheglov on 22.02.2022.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    var isContacted = false
+}
+
+@MainActor class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+    
+    init() {
+        self.people = []
+    }
+}
