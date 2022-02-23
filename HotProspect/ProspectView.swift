9fa8e20f4990633b86cdeee776949828cbe94601
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-23 13:11:13 +0600

    Add notifications

--- HotProspect/ProspectView.swift
+++ HotProspect/ProspectView.swift
@@ -7,6 +7,7 @@
 
 import CodeScanner
 import SwiftUI
+import UserNotifications
 
 struct ProspectView: View {
     
@@ -44,6 +45,13 @@ struct ProspectView: View {
                                 Label("Mark Contacted", systemImage: "person.crop.circle.fill.badge.checkmark")
                             }
                             .tint(.green)
+                            
+                            Button {
+                                addNotification(for: prospect)
+                            } label: {
+                                Label("Remind me", systemImage: "bell")
+                            }
+                            .tint(.orange)
                         }
                     }
                     
@@ -101,6 +109,39 @@ struct ProspectView: View {
         case .failure(let error):
             print("Scanning error: \(error.localizedDescription)")
         }
+    }
+    
+    func addNotification(for prospect: Prospect) {
+        let center = UNUserNotificationCenter.current()
+        
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.emailAddress
+            content.sound = .default
+            
+            var dateComponents = DateComponents()
+            dateComponents.hour = 9
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        }
+        
+        center.getNotificationSettings { setting in
+            if setting.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        addRequest()
+                    } else {
+                        print("D'oh!")
+                    }
+                }
+            }
+        }
+        
     }
 }
 
